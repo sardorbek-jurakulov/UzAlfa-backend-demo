233ecc184b653a24c7b08b7e84f1089197788c1d
sardorbek-jurakulov <sardorbek.juraqulov@gmail.com>
2022-01-20 14:47:43 +0500

    WIP

--- server.js
+++ server.js
@@ -24,8 +24,32 @@ app.get('/users/dashboard', (req, res) => {
   res.render('dashboard', { user: "Conor" });
 });
 
-app.post('/user/register', (req, res) => {
-  
+app.post('/users/register', (req, res) => {
+  let { name, email, password, password2 } = req.body;
+  console.log({
+    name, 
+    email,
+    password,
+    password2,
+  });
+
+  let errors = [];
+
+  if(!name || !email || !password || !password2) {
+    errors.push({message: "Please enter all fields!"});
+  }
+
+  if(password.length < 6) {
+    errors.push({message: "Password should be at leasr 6 characters"});
+  }
+
+  if(password !== password2) {
+    errors.push({message: "Passwords do not match"});
+  }
+
+  if(errors.length > 0) {
+    res.render('register', { errors });
+  }
 });
 
 app.listen(PORT, ()=>{
